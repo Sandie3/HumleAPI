e890b8daa34bd2f4f67fd9f6462158e569b3e114
Sandie3 <sandermunkh@gmail.com>
2021-09-07 14:01:18 +0200

    cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 4011;
@@ -19,12 +20,9 @@ db.once('open', (  ) => console.log("/// ----> MongoDB ready! "))
 
 // ---- APP
 // ------------------------------------------
-
 app.use( express.json() ); // Handle JSON data
 app.use( express.urlencoded( { extended: true } ) ) // Handle URLEncoded data
-
-
-
+app.use( cors( { credentials: true, origin: true } ) )
 
 // ---- SESSION
 // ------------------------------------------
